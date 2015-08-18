1dfd84925f6524749d40e76b799006448ea0e52f
Tony Francis <tonyfrancisv@gmail.com>
2015-08-17 23:10:18 -0400

    User System 	- Replaced name with first_name & last_name

--- db/migrate/20150817221206_add_username_to_users.rb
+++ db/migrate/20150817221206_add_username_to_users.rb
@@ -1,6 +1,7 @@
 class AddUsernameToUsers < ActiveRecord::Migration
   def change
     add_column :users, :username, :string, :null => false, :unique => true
-    add_column :users, :name, :string, :null => false
+    add_column :users, :first_name, :string, :null => false
+    add_column :users, :last_name, :string, :null => false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -96,7 +96,8 @@ ActiveRecord::Schema.define(version: 20150817221206) do
     t.datetime "updated_at",                          null: false
     t.integer  "crew_id"
     t.string   "username",                            null: false
-    t.string   "name",                                null: false
+    t.string   "first_name",                          null: false
+    t.string   "last_name",                           null: false
   end
 
   add_index "users", ["crew_id"], name: "index_users_on_crew_id", using: :btree
