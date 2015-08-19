99a3e3e8ea7e6d380fcc954e42c5be7085ba3788
Tony Francis <tonyfrancisv@gmail.com>
2015-08-18 23:16:42 -0400

    User System 	- Added logout link route for FB login

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
 
 
   devise_for :users, controllers: { registrations: "registrations", omniauth_callbacks: "users/omniauth_callbacks" }
+
+  devise_scope :user do
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
+  
   get 'listings/show'
 
   post 'listings/orbit'
