3d9d08aaa1aae009cc41dd14100b9953ebbbaa03
Tony Francis <tonyfrancisv@gmail.com>
2015-08-24 16:29:15 -0400

    User Controller 	- If id is passed to show method, show that user 	- Else, show current user 	- Requires login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   def show
+    if params[:id]
+      @user = User.find(params[:id])
+    else
+      @user = current_user
   end
 end
