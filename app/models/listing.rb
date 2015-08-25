df7eb4f32557be3471f3378af58fbee4c1f9eb60
Tony Francis <tonyfrancisv@gmail.com>
2015-08-25 17:46:24 -0400

    Common Scripts JS 	- Fixed nav login dropdown bug 	- Removed bootstrap JS code (bootstrap.js is handled by bootstrap gem)
    Listing Model
    	- Refined full_address method
    	- Uses lambda and blank? method for greater versatility

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -8,7 +8,7 @@ class Listing < ActiveRecord::Base
   belongs_to :owner, class_name: "User"
 
   def full_address
-    address_array = [address, address_2, city, state, zipcode, country] - [""]
+    address_array = [address, address_2, city, state, zipcode, country].reject(&:blank?)
     address_array.compact.join(', ')
   end
 
