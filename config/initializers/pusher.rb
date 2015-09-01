5c0345d9be7a78b503daa66b717ffb8fb3cf4c21
Tony Francis <tonyfrancisv@gmail.com>
2015-09-01 17:42:23 -0400

    Fixed pusher keys

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -4,10 +4,6 @@ Pusher.app_id = ENV['PUSHER_APP_ID']
 Pusher.key    = ENV['PUSHER_KEY']
 Pusher.secret = ENV['PUSHER_SECRET']
 
-Pusher.app_id = '138893'
-Pusher.key    = '87a966498799a786ec56'
-Pusher.secret = '22ed03c4fc3c9a1596c0'
-
 Pusher.url = "https://#{ENV['PUSHER_KEY']}:#{ENV['PUSHER_SECRET']}@api.pusherapp.com/apps/138893"
 
 Pusher.logger = Rails.logger
