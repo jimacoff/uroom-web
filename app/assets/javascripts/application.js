d3abf49c5cd973b9015bd41659e0e2e5cccacf6c
Tony Francis <tonyfrancisv@gmail.com>
2015-09-03 21:14:54 -0400

    Added dislike and sliderPro JS Added listing data to page
    Index Page
    	- modified search params

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.ddslick
+//= require jquery.sliderPro
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require slick
