d20b3116f86dc387124f28733b9eb63398a03120
Tony Francis <tonyfrancisv@gmail.com>
2015-09-10 02:14:34 -0400

    Registration Controller 	- generated files 	- Removed devise methods from helper; no longer needed

--- app/helpers/registrations_helper.rb
+++ app/helpers/registrations_helper.rb
@@ -1,17 +1,2 @@
 module RegistrationsHelper
-  def resource_name
-    :user
-  end
-
-  def resource
-    if current_user
-      @resource = current_user
-    else
-      @resource ||= current_user
-
-  end
-
-  def devise_mapping
-    @devise_mapping ||= Devise.mappings[:user]
-  end
 end
