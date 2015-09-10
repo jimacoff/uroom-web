4fafc02923036170562c0a6850ec395fc7ffab54
Tony Francis <tonyfrancisv@gmail.com>
2015-09-10 02:24:44 -0400

    Registrations Controller 	- Added handling of editing profile 	- editing profile does not require password

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,31 @@
 class RegistrationsController < Devise::RegistrationsController
+  before_action :authenticate_user!
+  include RegistrationsHelper
+
+
+  def edit_profile
+    if user_signed_in?
+      render :edit_profile
+    else
+      redirect_to user_session_path
+    end
+  end
+
+  def update_profile
+    current_user.about = params[:user][:about]
+    if current_user.save
+      redirect_to :edit_profile
+    else
+      render :edit_profile
+    end
+  end
+
+  protected
+
+
+  def after_update_path_for(resource)
+      edit_user_registration_path(resource)
+    end
 
   private
 
@@ -7,7 +34,9 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def account_update_params
-    params.require(:user).permit(:username, :first_name, :last_name, :email, :passowrd, :password_confirmation, :current_password)
+    params.require(:user).permit(:username, :first_name, :last_name, :email, :about, :passowrd, :password_confirmation, :current_password)
   end
+
+
 
 end
