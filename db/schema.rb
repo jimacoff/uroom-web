31907ef95e117a7017a412450bd85ea4ceb7189b
Tony Francis <tonyfrancisv@gmail.com>
2015-09-10 22:36:47 -0400

    Added avatar image to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150830214204) do
+ActiveRecord::Schema.define(version: 20150910235356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -194,6 +194,7 @@ ActiveRecord::Schema.define(version: 20150830214204) do
     t.string   "first_name",                             null: false
     t.string   "last_name",                              null: false
     t.boolean  "landlord",               default: false
+    t.boolean  "regular_user",           default: false
     t.text     "about",                  default: ""
     t.string   "merchant_account_id"
     t.string   "email",                  default: "",    null: false
@@ -215,6 +216,10 @@ ActiveRecord::Schema.define(version: 20150830214204) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "admin",                  default: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
