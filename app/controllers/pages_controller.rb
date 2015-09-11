a98125ecf8530faaee21774c18889c9634e62304
Tony Francis <tonyfrancisv@gmail.com>
2015-09-11 16:54:19 -0400

    Index Page now uses Month & Date selector
    Search controller gets date from new selector
    Array of locations

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
   # Controller to handle errors
   def index
+    @date = Date.today.at_beginning_of_month.next_month
+    @months = []
+    (0..11).each do |m|
+      @months << [@date.next_month(m).strftime("%b %Y"), @date.next_month(m)]
+    end
   end
 end
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,32 +5,33 @@ class SearchController < ApplicationController
   # 1 roommate listings, 2 roommate listings, 3 roommates, more
   def results
     # Get the paramters
-    @date = params[:date]
-    @month = params[:date]["month"].to_i
-    @year = params[:date]["year"].to_i
+
     @lease_length = params[:lease_length].to_i
     @roommates = params[:roommates].to_i if params[:roommates]
 
-    @start_date = start_date(@month, @year)
+    @start_date = params[:date].to_date
     @end_date = end_date(@start_date, @lease_length)
 
     @max_price_1 = params[:max].to_i * 2
     @max_price_2 = params[:max].to_i * 3
     @max_price_3 = params[:max].to_i * 4
 
-    @coordinates = Geocoder.coordinates(params[:location])
+    @locations = ["San Francisco, CA", "Foster City, CA", "New York City, NY", "Pittsburgh, PA"]
+    @location = @locations[params[:location].to_i]
+    @coordinates = Geocoder.coordinates(@location)
 
     # How to handle availability
     # Store available start and end dates (for more complex handling only store unavailable dates)
     # In query check if start date is greater or equal to and end is less than or equal to
 
     # Search Listings
-    # .where(['available_start >= ? AND available_end <= ?', @start_date, @end_date])
-    @listings = Listing.near("Pittsburgh, PA")
-    @listings_1 = Listing.near(params[:location])
-    @listings_2 = Listing.near(params[:location])
-    @listings_3 = Listing.near(params[:location])
-    @listings_more = Listing.near(params[:location])
+    # .where(['start_date >= ? AND end_date <= ?', @start_date, @end_date])
+    @listings   =     Listing.near(@location).where(['start_date >= ? AND end_date <= ?'], @start_date, @end_date)
+    @listings_1 =     Listing.near(@location).where(['start_date >= ? AND end_date <= ? AND price <= ?'], @start_date, @end_date, @max_price_1)
+    @listings_2 =     Listing.near(@location).where(['start_date >= ? AND end_date <= ? AND price <= ?'], @start_date, @end_date, @max_price_2)
+    @listings_3 =     Listing.near(@location).where(['start_date >= ? AND end_date <= ? AND price <= ?'], @start_date, @end_date, @max_price_3)
+    @listings_more =  Listing.near(@location).where(['start_date >= ? AND end_date <= ? AND price >= ?'], @start_date, @end_date, @max_price_3)
+    debugger
   end
 
 end
