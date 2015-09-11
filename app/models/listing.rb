1c6580729dc0f10eda036d675cf413902b388f9b
Tony Francis <tonyfrancisv@gmail.com>
2015-09-11 02:43:14 -0400

    Picture Model 	- image url and styles
    Listing Model
    	- has many pictures through gallery
    	- accepts nested attributes for gallery
    		- to destroy image from listing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,10 +3,13 @@ class Listing < ActiveRecord::Base
   after_validation :geocode
 
   has_one  :gallery
+  has_many :pictures, through: :gallery
   has_many :crews
   has_many :orbits
   has_many :users, through: :orbits
   belongs_to :owner, class_name: "User"
+
+  accepts_nested_attributes_for :gallery, allow_destroy: true
 
   def full_address
     address_array = [address, address_2, city, state, zipcode, country].reject(&:blank?)
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,7 +3,9 @@ class Picture < ActiveRecord::Base
 
   has_attached_file :image,
     :path => ":rails_root/public/images/:id/:filename",
-    :url  => "/images/:id/:filename"
+    :url  => "/images/:id/:filename",
+    styles: {large: "500x500", medium: "300x300>", thumb: "100x100>" },
+    default_url: "/images/:style/missing.png"
 
   do_not_validate_attachment_file_type :image
 end
