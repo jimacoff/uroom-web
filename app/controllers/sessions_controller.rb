219143586865a1a2e696289a3748c942ac1fe6f1
Tony Francis <tonyfrancisv@gmail.com>
2015-09-10 22:19:57 -0400

    Sessions Controller 	- Created 	- Subclasses Devise Sessions 	- Handles redirect after sign in

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < Devise::SessionsController
+  after_filter :store_location
+
+  def store_location
+    # store last url - this is needed for post-login redirect to whatever the user last visited.
+    return unless request.get?
+    if (request.path != "/users/sign_in" &&
+        request.path != "/users/sign_up" &&
+        request.path != "/users/password/new" &&
+        request.path != "/users/password/edit" &&
+        request.path != "/users/confirmation" &&
+        request.path != "/users/sign_out" &&
+        !request.xhr?) # don't store ajax calls
+      session[:previous_url] = request.fullpath
+    end
+  end
+
+  def after_sign_in_path_for(resource)
+    if resource.sign_in_count == 1
+       edit_user_registration_path(resource)
+    else
+       session[:previous_url] || root_path
+    end
+  end
+
+end
