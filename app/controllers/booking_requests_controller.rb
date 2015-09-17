70bc649322d2e22212ab7ce1f1e3e68482c95747
Tony Francis <tonyfrancisv@gmail.com>
2015-09-17 15:50:52 -0400

    Booking Requests Controller 	- Dash only shows requests that have not been accepted or rejected 	- If someone tries to access booking they don't have permission to, redirect 	- Accept - updates crew approved value, redirects to requests dash 	- Reject - tests crew requested to false, sets request's rejected to true, sends rejected email

--- app/controllers/booking_requests_controller.rb
+++ app/controllers/booking_requests_controller.rb
@@ -3,32 +3,38 @@ class BookingRequestsController < ApplicationController
 
   def index
     listings = Listing.where(owner: current_user)
-    @requests = BookingRequest.where(listing: listings)
+    @requests = BookingRequest.where(listing: listings, rejected: false, accepted: false)
     render 'dashboard/bookingrequests'
   end
 
   def show
     @request = BookingRequest.find(params[:id])
     if @request.listing.owner != current_user
-      # redirect to requests dashboard
+      direct_to bookingrequests_path
     end
   end
 
+  # Start group email
+  # Mark request as accepted
   def accept
-    # Start group email
-    # Mark request as accepted
     @request = BookingRequest.find(params[:id])
     @request.update(accepted: true)
+    @request.crew.update(approved: true)
     recipients = @request.crew.users + [@request.listing.owner]
     BookingMailer.booking_group_email(recipients, @request).deliver_now
     flash[:success] = "Accepted"
-    redirect_to @request.listing
-    # redirect_to booking requests dashboard
+    redirect_to bookingrequests_path
   end
 
+  # Send email saying was not approved
+  # Mark request as rejected
   def reject
-    # Send email saying was not approved
-    # Mark request as rejected
-    # You can always submit another request
+    @request = BookingRequest.find(params[:id])
+    @request.update(rejected: true)
+    @request.crew.update(requested: false)
+    recipients = @request.crew.users
+    BookingMailer.booking_rejected_email(recipients, @request).deliver_now
+    flash[:success] = "Rejected"
+    redirect_to bookingrequests_path
   end
 end
