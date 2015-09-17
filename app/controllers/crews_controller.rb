aea93f49a2a612c4d97108baa665d90b2121a6eb
Tony Francis <tonyfrancisv@gmail.com>
2015-09-17 16:20:23 -0400

    Crews Controller 	- Refined create crew 	- Refined add user, can now add user by email (invites if not a user) 	- Refined remove user 	- Commented out unused methods

--- app/controllers/crews_controller.rb
+++ app/controllers/crews_controller.rb
@@ -1,66 +1,61 @@
 class CrewsController < ApplicationController
   before_action :authenticate_user!
+
   # Create a crew. Done on a listing page.
-
   def create
-    crew = Crew.new
-    crew.crew_admin = current_user
-    crew.users << current_user
     orbit = Orbit.find(params[:orbit])
+    crew = Crew.create(crew_admin: current_user, users: [current_user], listing: orbit.listing, start_date: orbit.start_date, end_date: orbit.end_date)
 
-    orbit.crew = crew
-    #orbit.save
-
-    crew.listing = orbit.listing
-    crew.start_date = orbit.start_date
-    crew.end_date = orbit.end_date
-
-    crew_requests = []
-    params[:users].each do |user_id|
-      crew_request = CrewRequest.new
+    params[:users].split(',').each do |user_id|
       requested_user = User.find(user_id)
-      crew_request.user_id = requested_user.id if requested_user
-      crew_request.crew = crew
-
-      crew_requests << crew_request
+      crew_request = CrewRequest.create(user: requested_user, crew: crew) if requested_user
     end
 
-    if crew.save
-      crew_requests.each do |request| request.save  end
-      orbit.update_attributes(has_crew: true) # current_user's orbit
+    params[:emails].split(' ').each do |email|
+      user = User.find_by(email: email)
+      crew_request = CrewRequest.create(user: user, crew: crew)
+      if user.nil?
+        user = User.invite_friend!(email: email, username: email, first_name: "", last_name: "", invited_crew: crew_request, regular_user: true)
+        crew_request.update(user: user)
+      end
+    end
+
+    if orbit.update_attributes(crew: crew)
       flash[:success] = "Sent crew requests!"
     else
-      # render listing page
       flash[:error] = "Failed to create crew."
     end
     redirect_to crew.listing
   end
 
+
   # Create new crew request
   def add_user
-    user = User.find(params[:user])
-    crew = Crew.find(params[:id])
-
-    crew_request = CrewRequest.new
-    crew_request.user = user
-    crew_request.crew = crew
-
-    if crew_request.save
-      # redirect to listing
-    else
-      # render page
+    crew = Crew.find(params[:crew])
+    params[:emails].split(' ').each do |email|
+      user = User.find_by(email: email)
+      crew_request = CrewRequest.create(crew: crew, user: user)
+      if user.nil?
+        username = email.split("@").first
+        user = User.invite_friend!(email: email, username: username, first_name: "", last_name: "", invited_crew: crew_request, regular_user: true)
+        crew_request.update(user: user)
+      end
+      crew.crew_requests << crew_request
     end
+
+    if crew.save
+      flash[:success] = "Invited people to group"
+    else
+      flash[:error] = "Could not invite people to group"
+    end
+    redirect_to crew.listing
   end
 
   def remove_user
     crew = Crew.find(params[:crew])
     user = user.find(params[:user])
 
-    if crew.admin == current_user
-      # Remove user from crew
-      crew.users.delete(user)
-      crew.save
-
+    if crew.crew_admin == current_user
       crew.users.each do |crew_member|
         orbit = Orbit.find_by(user: crew_member, listing: crew.listing)
         if crew_member == user # If the user is the one that was removed
@@ -70,9 +65,13 @@ class CrewsController < ApplicationController
         oribt.landed = false
         orbit.save
       end
+      crew.users.delete(user)
+      crew.save
     else
-      # render you don't have permission
+      flash[:error] = "You don't have permission to do that"
     end
+    
+    redirect_to crew.listing
   end
 
   # Remove the current, logged in user from the crew
@@ -87,90 +86,87 @@ class CrewsController < ApplicationController
     crew.users.each do |user|
       orbit = Orbit.find_by(user: user, listing: listing)
       if user == current_user
-        orbit.has_crew = false
-        orbit.crew = nil
+        orbit.update_attributes(crew: nil)
         crew.users.delete(current_user)
       end
-      orbit.ready_to_land = false
-      orbit.landed = false
-      orbit.save
-    end
-
-    if crew.crew_admin == current_user
-      crew.crew_admin = crew.users.first
+      orbit.update_attributes(ready_to_land: false, landed: false)
     end
 
     if crew.users.count == 0
+      crew.crew_requests.each do |request| request.destroy end
       crew.destroy
+    elsif crew.crew_admin == current_user
+      crew.crew_admin = crew.users.first
+      crew.save
     end
 
     redirect_to listing
   end
 
-  def ready_to_land
-    @crew = Crew.find(:crew)
-
-    orbit = Orbit.find_by(user: current_user, listing: @crew.listing)
-    orbit.ready_to_land = true
-    orbit.save
-
-    all_are_ready = true
-    @crew.users.each do |user|
-      orbit = Orbit.find_by(user: user, listing: crew.listing)
-      if orbit.ready_to_land == false
-        all_are_ready = false
-      end
-    end
-
-    if orbit.save
-      if all_are_ready
-        @crew.ready_to_land = true
-        if @crew.save
-          request_landing
-        else
-          redirect_to @crew.listing
-        end
-      else
-        flash[:success] = "You have been set to ready"
-        redirect_to @crew.listing
-      end
-    else
-      flash[:error] = "Could not set ready"
-      redirect_to @crew.listing
-    end
-  end
-
-  def not_ready_to_land
-    crew = Crew.find(:crew)
-
-    orbit = Orbit.find_by(user: current_user, listing: @crew.listing)
-    orbit.ready_to_land = false
-
-    if orbit.save
-      flash[:notice] = "You are no longer ready to land"
-    else
-      flash[:error] = "Could not set as not ready"
-    end
-    redirect_to crew.listing
-  end
-
-  def request_landing
-    @crew = Crew.find(:crew) if params[:crew]
-
-    approval_request = CrewApprovalRequest.new
-    approval_request.crew         = @crew
-    approval_request.listing      = @crew.listing
-    approval_request.landlord     = @crew.listing.owner
-    approval_request.start_date   = @crew.start_date
-    approval_request.lease_length = @crew.lease_length
-
-    if approval_request.save
-      flash[:success] = "All users are ready, leasing request made"
-    else
-      flash[:error] = "Landing request could not be made"
-    end
-
-    redirect_to @crew.listing
-  end
+  # def ready_to_land
+  #   @crew = Crew.find(:crew)
+  #
+  #   orbit = Orbit.find_by(user: current_user, listing: @crew.listing)
+  #   orbit.ready_to_land = true
+  #   orbit.save
+  #
+  #   all_are_ready = true
+  #   @crew.users.each do |user|
+  #     orbit = Orbit.find_by(user: user, listing: crew.listing)
+  #     if orbit.ready_to_land == false
+  #       all_are_ready = false
+  #     end
+  #   end
+  #
+  #   if orbit.save
+  #     if all_are_ready
+  #       @crew.ready_to_land = true
+  #       if @crew.save
+  #         request_landing
+  #       else
+  #         redirect_to @crew.listing
+  #       end
+  #     else
+  #       flash[:success] = "You have been set to ready"
+  #       redirect_to @crew.listing
+  #     end
+  #   else
+  #     flash[:error] = "Could not set ready"
+  #     redirect_to @crew.listing
+  #   end
+  # end
+  #
+  # def not_ready_to_land
+  #   crew = Crew.find(:crew)
+  #
+  #   orbit = Orbit.find_by(user: current_user, listing: @crew.listing)
+  #   orbit.ready_to_land = false
+  #
+  #   if orbit.save
+  #     flash[:notice] = "You are no longer ready to land"
+  #   else
+  #     flash[:error] = "Could not set as not ready"
+  #   end
+  #   redirect_to crew.listing
+  # end
+  #
+  # def request_landing
+  #   @crew = Crew.find(:crew) if params[:crew]
+  #
+  #   approval_request = CrewApprovalRequest.new
+  #   approval_request.crew         = @crew
+  #   approval_request.listing      = @crew.listing
+  #   approval_request.landlord     = @crew.listing.owner
+  #   approval_request.start_date   = @crew.start_date
+  #   approval_request.lease_length = @crew.lease_length
+  #
+  #   if approval_request.save
+  #     flash[:success] = "All users are ready, leasing request made"
+  #   else
+  #     flash[:error] = "Landing request could not be made"
+  #   end
+  #
+  #   redirect_to @crew.listing
+  # end
 
 end
