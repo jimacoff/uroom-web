f76d5eea209628533fec3ffe6efb27623779f6b9
Tony Francis <tonyfrancisv@gmail.com>
2015-10-03 15:34:50 -0400

    Changed start date for search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   # Controller to handle errors
   def index
-    @date = Date.today.at_beginning_of_month.next_month
+    @date = Date.today.at_beginning_of_month.next_year
     @months = []
     (0..11).each do |m|
       @months << [@date.next_month(m).strftime("%b %Y"), @date.next_month(m)]
